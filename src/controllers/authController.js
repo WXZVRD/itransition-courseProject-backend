d1dfff8368832de6c8fde1a959dfa5a06de1ee10
WXZVRD <134217021+WXZVRD@users.noreply.github.com>
2023-09-23 15:34:17 +0600

    Update authController.js

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -53,7 +53,7 @@ module.exports = {
                     domain: 'itransition-courseproject-backend.onrender.com', 
                     secure: true,
                     sameSite: 'none',
-                    httpOnly: true
+                    httpOnly: false
                   }
                 );
                 res.cookie('jwt', user.token, 
@@ -61,7 +61,7 @@ module.exports = {
                     domain: 'itransition-courseproject-backend.onrender.com', 
                     secure: true,
                     sameSite: 'none',
-                    httpOnly: true
+                    httpOnly: false
                   }
                 );
                 res.redirect('https://itransition-course-project-frontend.vercel.app')
